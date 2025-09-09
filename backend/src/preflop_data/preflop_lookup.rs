e8813242473c21ae67001f3bcc9d3ebb4997baaf
Kristofferjoha <kristofferjoha@gmail.com>
2025-09-09 14:04:11 +0200

    comments etc

--- backend/src/preflop_data/preflop_lookup.rs
+++ backend/src/preflop_data/preflop_lookup.rs
@@ -1,5 +1,7 @@
 use serde::Deserialize;
 
+/// Represents the equity of hand1 against hand2 in a preflop scenario from preflop_equity.json.
+
 #[derive(Deserialize, Debug, Clone)]
 pub struct PreflopEquity {
     pub hand1: String,

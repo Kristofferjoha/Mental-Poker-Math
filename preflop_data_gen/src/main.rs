0bd8b291de2f998b3f17ebfdae79525859635575
Kristofferjoha <kristofferjoha@gmail.com>
2025-09-09 20:51:19 +0200

    data 1 mil sims each

--- preflop_data_gen/src/main.rs
+++ preflop_data_gen/src/main.rs
@@ -8,7 +8,7 @@ use tools::generator::preflop_equity_generation;
 /// Command line program for calculating preflop equity for starting hands.
 
 fn main() -> std::io::Result<()> {
-    const NUM_OF_SIMULATIONS: u32 = 10000;
+    const NUM_OF_SIMULATIONS: u32 = 1000000;
 
     preflop_equity_generation(NUM_OF_SIMULATIONS)?;
     Ok(())

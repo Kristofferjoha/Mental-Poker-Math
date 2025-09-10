b20292e4c9b1292c730fe28bf4e049b4186438ad
Kristofferjoha <kristofferjoha@gmail.com>
2025-09-10 16:05:20 +0200

    production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		proxy: {
-			'/api': 'http://localhost:3000'
-		}
-	}
+    plugins: [sveltekit()],
+    server: {
+        proxy: {
+            // Forward all requests starting with /api to your live backend
+            '/api': {
+                target: 'http://91.99.122.106:8001',
+                changeOrigin: true, // This is important for properly forwarding the request
+            }
+        }
+    }
 });
